c89a2763f051d390b064d973a747998cbcddbf83
leonardobaranelli <leobaranelli@gmail.com>
2024-02-29 10:51:42 -0300

    yes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import * as path from 'path';
 import { setupSwagger } from '../swagger-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   setupSwagger(app);
+
+  app.use('/', express.static(path.join(__dirname, '..', 'public', 'swagger')));
+
   await app.listen(3000);
 }
+
 bootstrap();
