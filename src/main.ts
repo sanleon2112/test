4aee676162f098559ea81378183ea31880747a93
leonardobaranelli <leobaranelli@gmail.com>
2024-02-29 10:39:32 -0300

    asdf32

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import * as path from 'path';
-
+//
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
