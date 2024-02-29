9d750970c93441a0b041a1f972237bf9ba3ca64e
leonardobaranelli <leobaranelli@gmail.com>
2024-02-29 08:45:57 -0300

    asdflkjlkj123

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,6 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
 import { UsersAPI } from "./users/entities/usersAPI.entity";
 import { ConfigModule } from '@nestjs/config';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as express from 'express';
-import * as path from 'path';
 
 @Module({
   imports: [
@@ -31,20 +28,4 @@ import * as path from 'path';
   controllers: [],
   providers: [],
 })
-export class AppModule {
-  constructor() {
-    const app = express();
-    
-    app.use('/swagger', express.static(path.join(__dirname, '../public/swagger')));
-
-    const options = new DocumentBuilder()
-      .setTitle('Your API Title')
-      .setDescription('Your API Description')
-      .setVersion('1.0')
-      .addTag('your-tag')
-      .build();
-
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('swagger', app, document);
-  }
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use('/swagger', express.static(path.join(__dirname, '..', 'public', 'swagger')));
+
   await app.listen(3000);
 }
+
 bootstrap();
