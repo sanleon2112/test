86de59d76e4bac75371ff98ea3b0947af301559a
leonardobaranelli <leobaranelli@gmail.com>
2024-02-29 08:13:56 -0300

    asdfasdf

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
+//
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -14,23 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('/api-docs', app, document, {
-    swaggerUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.8',
-    swaggerOptions: {
-      urls: [
-        {
-          url: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.8/swagger-ui.css',
-          name: 'swagger-ui.css'
-        },
-        {
-          url: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.8/swagger-ui-bundle.js',
-          name: 'swagger-ui-bundle.js'
-        },
-        {
-          url: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.8/swagger-ui-standalone-preset.js',
-          name: 'swagger-ui-standalone-preset.js'
-        }
-      ]
-    }
+    swaggerUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.8'    
   });
 
   // Manejar redirección en la raíz
